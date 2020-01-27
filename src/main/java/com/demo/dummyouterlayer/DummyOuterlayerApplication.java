f7eeff61ed3faeeb63e671aab6de580407fd2015
Abhishek <abhishek21111987@gmail.com>
2020-01-27 15:08:31 -0500

    commit 4

--- src/main/java/com/demo/dummyouterlayer/DummyOuterlayerApplication.java
+++ src/main/java/com/demo/dummyouterlayer/DummyOuterlayerApplication.java
@@ -4,8 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.demo.dummyouterlayer.controller.EmployeeController;
+import com.demo.dummyouterlayer.service.EmployeeService;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = EmployeeController.class)
+@ComponentScan(basePackageClasses = EmployeeService.class)
 public class DummyOuterlayerApplication extends SpringBootServletInitializer {
 
 	 @Override
