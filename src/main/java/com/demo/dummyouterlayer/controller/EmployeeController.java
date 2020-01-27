a01fb0e1cce614f124dba5fde47423c72f484cc4
Abhishek <abhishek21111987@gmail.com>
2020-01-27 15:19:40 -0500

    commit6

--- src/main/java/com/demo/dummyouterlayer/controller/EmployeeController.java
+++ src/main/java/com/demo/dummyouterlayer/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.dummyouterlayer.model.Employee;
@@ -20,5 +21,11 @@ public class EmployeeController {
 		return employeeService.retrieveEmployeeInfo(empId);
 
 	}
+	
+	@RequestMapping("/test")
+    public String getTest()
+    {
+        return "Test!";
+    }
 
 }
