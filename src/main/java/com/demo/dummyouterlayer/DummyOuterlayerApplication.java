3ab416aa268a5ec65b0b69fa1c787a4904ec672d
Abhishek <abhishek21111987@gmail.com>
2020-01-27 14:49:01 -0500

    commit 3

--- src/main/java/com/demo/dummyouterlayer/DummyOuterlayerApplication.java
+++ src/main/java/com/demo/dummyouterlayer/DummyOuterlayerApplication.java
@@ -2,10 +2,16 @@ package com.demo.dummyouterlayer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DummyOuterlayerApplication {
+public class DummyOuterlayerApplication extends SpringBootServletInitializer {
 
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(DummyOuterlayerApplication.class);
+	    }
 	public static void main(String[] args) {
 		SpringApplication.run(DummyOuterlayerApplication.class, args);
 	}
